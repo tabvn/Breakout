4412a1c0818870212f3d511faffcde8e9ee56557
tiennl <nlthtien@gmail.com>
2021-07-04 17:07:32 +0700

    add level

--- js/main.js
+++ js/main.js
@@ -27,19 +27,22 @@ var fps = 60;
 var fpsInterval = 1000 / fps;
 var then = Date.now();
 var startTime = then;
-var delta = 30;
+var delta = 50;
 var now;
 var deltaX = dx * (delta/100);
 var deltaY = dy * (delta/100);
 
+var brickStatus = JSON.parse(data);
+
+var brickTotal = [];
+var level = 0;
+var maxLevel = 4;
+var scoreTotal = 0;
+var scoreCurrentLevel = 0;
+
 var bricks = [];
-for (var c=0; c<brickColumnCount; c++) {
-    bricks[c] = [];
-    for (var r=0; r<brickRowCount; r++) {
-        brickColor = getRandomColor();
-        bricks[c][r] = { x: 0, y: 0, color: brickColor, status: 1 };
-    }
-}
+
+getBrickToTal();
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
@@ -111,10 +114,24 @@ function collisionDetection() {
                 if (RectCircleColliding(circle, rect)) {
                     dy = -dy;
                     b.status = 0;
-                    score++;
-                    if (score == brickRowCount*brickColumnCount) {
-                        alert("YOU WIN, CONGRATULATIONS!" + "\n" + "YOU GOT " + score + " SCORE!");
-                        document.location.reload();
+                    scoreCurrentLevel++;
+                    scoreTotal++;
+                    if (scoreCurrentLevel == brickTotal[level]) {
+                        if (level == maxLevel - 1) {
+                            alert("YOU WIN, CONGRATULATIONS!" + "\n" + "YOU GOT " + scoreTotal + " SCORES!");
+                            document.location.reload();
+                        }
+                        else {
+                            alert("YOU HAVE PASS LEVEL " + (level+1) + "\n" + "CLICK OK TO PLAY NEXT LEVEL!");
+                            level++;
+                            getBrick(level);
+                            x = canvas.width/2;
+                            y = canvas.height-30;
+                            draw();
+                            // requestAnimationFrame(draw);
+                            scoreCurrentLevel = 0;
+
+                        }
                     }
                 }
             }
@@ -154,7 +171,7 @@ function collisionDetection() {
         console.log("BALL FALL OUT");
         lives--;
         if (!lives) {
-            alert("GAME OVER");
+            alert("GAME OVER" + "\ln" + "YOUR SCORE: " + scoreTotal);
             document.location.reload();
         }
         else {
@@ -184,6 +201,35 @@ function getRandomColor() {
     return 'rgb(' + r + ',' + g + ',' + b + ')';
 }
 
+function getBrickToTal() {
+    for (var i=0; i<maxLevel; i++)
+        brickTotal.push(countBrickLevel(i));
+}
+
+function countBrickLevel(i) {
+    var countBrick = 0;
+    for (var c=0; c<brickColumnCount; c++) {
+        bricks[c] = [];
+        for (var r=0; r<brickRowCount; r++) {
+            brickColor = getRandomColor();
+            bricks[c][r] = { x: 0, y: 0, color: brickColor, status: brickStatus[i][r][c] };
+            if (brickStatus[i][r][c] === 1) 
+                countBrick++;
+        }
+    }
+    return countBrick;
+}
+
+function getBrick(i){
+    for (var c=0; c<brickColumnCount; c++) {
+        bricks[c] = [];
+        for (var r=0; r<brickRowCount; r++) {
+            brickColor = getRandomColor();
+            bricks[c][r] = { x: 0, y: 0, color: brickColor, status: brickStatus[i][r][c] };
+        }
+    }
+}
+
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
@@ -201,6 +247,7 @@ function drawPaddle() {
 }
 
 function drawBricks() {
+    // getBrick(level);
     for (var c=0; c<brickColumnCount; c++) {
         for (var r=0; r<brickRowCount; r++) {
             if (bricks[c][r].status == 1) {
@@ -221,7 +268,13 @@ function drawBricks() {
 function drawScore() {
     ctx.font = "16px Arial";
     ctx.fillStyle = "#0095DD";
-    ctx.fillText("Score: "+score, 8, 20);
+    ctx.fillText("Score: "+scoreTotal, 8, 20);
+}
+
+function drawLevel() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Level: "+ (level+1), canvas.width/2-30, 20);
 }
 
 function drawLives() {
@@ -237,6 +290,7 @@ function draw() {
     drawPaddle();
     drawScore();
     drawLives();
+    drawLevel();
     collisionDetection();
     
 
@@ -256,4 +310,5 @@ function draw() {
     else delta = 0;
 }
 
+getBrick(level);
 draw();
